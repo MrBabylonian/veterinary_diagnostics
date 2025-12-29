5c845467a77de26db3fdad944b5894dd7628b449
MrBabylonian <bedirhangilgiler@gmail.com>
2025-12-29 11:25:51 +0100

    feat: implement landing page layout with backgrounds, components, and footer Refactor LandingPage component to include background layers, navbar, hero, features, and footer Update page.tsx to use the new LandingPage component Add new Feature component Update demo HTML files with formatting improvements and minor text changes Minor CSS comment update in globals.css

--- frontend/client/src/app/page.tsx
+++ frontend/client/src/app/page.tsx
@@ -1,12 +1,10 @@
 // app/page.tsx
-import { Hero } from "@/components/landing/Hero";
-import { Navbar } from "@/components/landing/Navbar";
+import LandingPage from "@/components/landing/LandingPage";
 
 export default function Home() {
   return (
-    <div>
-      <Navbar />
-      <Hero />
-    </div>
+    <>
+      <LandingPage />
+    </>
   );
 }
